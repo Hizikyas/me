bd58a69c8e9606f6ad2b346917dbe5647098e887
Hizikyas <122904913+Hizikyas@users.noreply.github.com>
2023-05-18 18:01:45 +0300

    exercises

---
+++ poiniter.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+ using namespace std;
+ 
+
+ int main ()
+ {
+    int age, list[6], ordered[6];
+    cout<<"Enter age of six students : \n";
+    for (int i=0;   i<6;   i++)
+    {
+      cin>>age;
+      list[i]=age;
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < 6; j++)
+        {
+            if (list[i] == list[j])
+            {
+                /* code */
+            }
+            if (list[i] < list[j])
+            {
+                count++;
+            }  
+        }
+        
+        for (int j = 0; j < 6; j++)
+        {
+            if (count == j)
+                ordered[j] = list[i];
+        }
+        
+        
+    }
+    
+    cout<<"------------------------ordered----------------------------\n";
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<ordered[i]<<",  ";
+    }
+    
+
+    return 0;
+ }
